7726def6eab8b39ddb2ce8600cb08635f5d5454b
doorcs <itsdoorcs@gmail.com>
2025-07-24 16:14:38 +0900

    feat: 인가 코드 받기 구현

--- src/main/java/gift/controller/MemberController.java
+++ src/main/java/gift/controller/MemberController.java
@@ -1,16 +1,23 @@
 package gift.controller;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gift.dto.LoginRequest;
 import gift.dto.LoginResponse;
 import gift.dto.RegisterRequest;
 import gift.service.MemberService;
+
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -37,5 +44,22 @@ public class MemberController {
     ) {
         LoginResponse response = memberService.signin(request);
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/oauth2/kakao") // http://localhost:8080/api/members/oauth2/kakao
+    public void kakaoLogin(
+        @Value("${auth.oauth2.kakao.client-id}") String clientId,
+        @Value("${auth.oauth2.kakao.client-uri}") String clientUri,
+        @Value("${auth.oauth2.kakao.redirect-uri}") String redirectUri,
+        HttpServletResponse response
+    ) throws IOException {
+        response.sendRedirect(
+            clientUri + clientId + "&redirect_uri=" + redirectUri + "&response_type=code"
+        );
+    }
+
+    @GetMapping("/oauth2/kakao/callback")
+    public ResponseEntity<String> kakaoLoginCallback(@RequestParam String code){
+        return ResponseEntity.status(HttpStatus.OK).body(code);
     }
 }
